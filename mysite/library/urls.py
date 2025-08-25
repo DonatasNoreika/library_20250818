cbad48ca656c7759506edce5770d4e61861d2e87
DonatasNoreika <donoras@gmail.com>
2025-08-25 18:57:44 +0300

    5: Pridėtas knygų sąrašo puslapis

--- mysite/library/urls.py
+++ mysite/library/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import index, authors, author
+from .views import BookListView
 
 urlpatterns = [
     path('', index, name='index'),
     path('authors/', authors, name='authors'),
     path('authors/<int:author_id>', author, name="author"),
+    path('books/', BookListView.as_view(), name='books'),
 ]
