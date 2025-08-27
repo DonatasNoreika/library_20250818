3b62d4df6cdb001ed36b62c34f940434277649d7
DonatasNoreika <donoras@gmail.com>
2025-08-27 19:27:02 +0300

    7: Pakeisti slaptažodžio pakeitimo šablonai

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,6 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-h-a+^zsk94)dccp*tz4*y^9ux)6w@&hw*^la5nnv@i*6dk4kv*'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mano_pastas@gmail.com'
+EMAIL_HOST_PASSWORD = 'VerySecret'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
